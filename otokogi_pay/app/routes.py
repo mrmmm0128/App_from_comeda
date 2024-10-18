46706f39fc4c8381a40a68540b9fbc282b9853a2
souta21 <soutaginta@gmail.com>
2024-10-19 00:15:02 +0900

    Add files via upload

--- otokogi_pay/app/routes.py
+++ otokogi_pay/app/routes.py
@@ -1,6 +1,14 @@
 from flask import render_template, request, jsonify
 from app import app
+from app.seisan import seisan
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
-    return render_template('index.html')
+    matching = None
+
+    if request.method == 'POST':
+        pay_dict = request.get_json()
+        concequence = seisan(pay_dict)
+        return jsonify({"payment":concequence})
+
+    return render_template('base.html')
